572ef9c4ee83a8ecfe2a585ffbbfef8a12fa3073
rorylongwell <rory.longwell@gmail.com>
2018-04-17 21:56:25 +0100

    Meal selection changes

--- KenoAssist.Web/Controllers/FoodAndDrinkController.cs
+++ KenoAssist.Web/Controllers/FoodAndDrinkController.cs
@@ -13,57 +13,57 @@ namespace KenoAssist.Web.Controllers
 {
     public class FoodAndDrinkController : Controller
     {
-        private List<FoodCheckBoxModel> breakfastCheckbox;
-        private List<FoodCheckBoxModel> lunchCheckbox;
-        private List<FoodCheckBoxModel> dinnerCheckbox;
+		private List<FoodModel> breakfastList;
+		private List<FoodModel> lunchList;
+		private List<FoodModel> dinnerList;
 
-        private List<FoodCheckBoxModel> selectedBreakfast;
-        private List<FoodCheckBoxModel> selectedLunch;
-        private List<FoodCheckBoxModel> selectedDinner;
+		private List<FoodModel> selectedBreakfast;
+		private List<FoodModel> selectedLunch;
+		private List<FoodModel> selectedDinner;
 
-        IEnumerable<FoodIntakeModel> menu;
+		IEnumerable<FoodIntakeModel> menu;
 
         public FoodAndDrinkController()
         {
-            breakfastCheckbox = new List<FoodCheckBoxModel>()
+			breakfastList = new List<FoodModel>()
             {
-                new FoodCheckBoxModel(){Id = 1, Name = "Toast and jam", FoodTypeId = 1,IsChecked = false},
-                new FoodCheckBoxModel(){Id = 2, Name = "Scrambled eggs and toast", FoodTypeId = 1,IsChecked = false},
-                new FoodCheckBoxModel(){Id = 3, Name = "Porridge (lactose free milk)", FoodTypeId = 1,IsChecked = false},
-                new FoodCheckBoxModel(){Id = 4, Name = "Rice Krispies (lactose free milk)", FoodTypeId = 1,IsChecked = false},
-                new FoodCheckBoxModel(){Id = 5, Name = "Pancakes and jam", FoodTypeId = 1,IsChecked = false},
-                new FoodCheckBoxModel(){Id = 6, Name = "Apple slices", FoodTypeId = 2,IsChecked = false},
-                new FoodCheckBoxModel(){Id = 7, Name = "Lacotose free strawberry yogurt", FoodTypeId = 2,IsChecked = false},
-                new FoodCheckBoxModel(){Id = 8, Name = "Large orange", FoodTypeId = 2,IsChecked = false}
+				new FoodModel(){Id = 1, Name = "Toast and jam", FoodTypeId = 1},
+				new FoodModel(){Id = 2, Name = "Scrambled eggs and toast", FoodTypeId = 1},
+				new FoodModel(){Id = 3, Name = "Porridge (lactose free milk)", FoodTypeId = 1},
+				new FoodModel(){Id = 4, Name = "Rice Krispies (lactose free milk)", FoodTypeId = 1},
+				new FoodModel(){Id = 5, Name = "Pancakes and jam", FoodTypeId = 1},
+				new FoodModel(){Id = 6, Name = "Apple slices", FoodTypeId = 2},
+				new FoodModel(){Id = 7, Name = "Lacotose free strawberry yogurt", FoodTypeId = 2},
+				new FoodModel(){Id = 8, Name = "Large orange", FoodTypeId = 2}
             };
 
-            lunchCheckbox = new List<FoodCheckBoxModel>()
+			lunchList = new List<FoodModel>()
             {
-                new FoodCheckBoxModel(){Id = 9, Name = "Vegetable soup and a bap", FoodTypeId = 1,IsChecked = false},
-                new FoodCheckBoxModel(){Id = 10, Name = "Sausage rolls", FoodTypeId = 1,IsChecked = false},
-                new FoodCheckBoxModel(){Id = 11, Name = "Baked potato, chicken and lactose free cheese", FoodTypeId = 1,IsChecked = false},
-                new FoodCheckBoxModel(){Id = 12, Name = "Chicken sandwich", FoodTypeId = 1,IsChecked = false},
-                new FoodCheckBoxModel(){Id = 13, Name = "Tuna and onion sandwich", FoodTypeId = 1,IsChecked = false},
-                new FoodCheckBoxModel(){Id = 14, Name = "Melon selection", FoodTypeId = 2,IsChecked = false},
-                new FoodCheckBoxModel(){Id = 15, Name = "Lacotose free strawberry yogurt", FoodTypeId = 2,IsChecked = false},
-                new FoodCheckBoxModel(){Id = 16, Name = "Sttrawberry jelly", FoodTypeId = 2,IsChecked = false}
+				new FoodModel(){Id = 1, Name = "Vegetable soup and a bap", FoodTypeId = 1},
+				new FoodModel(){Id = 2, Name = "Sausage rolls", FoodTypeId = 1},
+				new FoodModel(){Id = 3, Name = "Baked potato, chicken and lactose free cheese", FoodTypeId = 1},
+				new FoodModel(){Id = 4, Name = "Chicken sandwich", FoodTypeId = 1},
+				new FoodModel(){Id = 5, Name = "Tuna and onion sandwich", FoodTypeId = 1},
+				new FoodModel(){Id = 6, Name = "Melon selection", FoodTypeId = 2},
+				new FoodModel(){Id = 7, Name = "Lacotose free strawberry yogurt", FoodTypeId = 2},
+				new FoodModel(){Id = 8, Name = "Strawberry jelly", FoodTypeId = 2}
             };
 
-             dinnerCheckbox = new List<FoodCheckBoxModel>()
+			dinnerList = new List<FoodModel>()
             {
-                new FoodCheckBoxModel(){Id = 17, Name = "Chicken curry and rice", FoodTypeId = 1,IsChecked = false},
-                new FoodCheckBoxModel(){Id = 18, Name = "Fish and chips", FoodTypeId = 1,IsChecked = false},
-                new FoodCheckBoxModel(){Id = 19, Name = "Tuna salad", FoodTypeId = 1,IsChecked = false},
-                new FoodCheckBoxModel(){Id = 20, Name = "Sausage and chips", FoodTypeId = 1,IsChecked = false},
-                new FoodCheckBoxModel(){Id = 21, Name = "Beef stew", FoodTypeId = 1,IsChecked = false},
-                new FoodCheckBoxModel(){Id = 22, Name = "Lacotose free ice-cream", FoodTypeId = 2,IsChecked = false},
-                new FoodCheckBoxModel(){Id = 23, Name = "Lacotose free custard and cake", FoodTypeId = 2,IsChecked = false},
-                new FoodCheckBoxModel(){Id = 24, Name = "Sttrawberry jelly", FoodTypeId = 2,IsChecked = false}
+				new FoodModel(){Id = 1, Name = "Chicken curry and rice", FoodTypeId = 1},
+				new FoodModel(){Id = 2, Name = "Fish and chips", FoodTypeId = 1},
+				new FoodModel(){Id = 3, Name = "Tuna salad", FoodTypeId = 1},
+				new FoodModel(){Id = 4, Name = "Sausage and chips", FoodTypeId = 1},
+				new FoodModel(){Id = 5, Name = "Beef stew", FoodTypeId = 1},
+				new FoodModel(){Id = 6, Name = "Lacotose free ice-cream", FoodTypeId = 2},
+				new FoodModel(){Id = 7, Name = "Lacotose free custard and cake", FoodTypeId = 2},
+				new FoodModel(){Id = 8, Name = "Sttrawberry jelly", FoodTypeId = 2}
             };
 
-            selectedBreakfast = new List<FoodCheckBoxModel>();
-            selectedLunch = new List<FoodCheckBoxModel>();
-            selectedDinner = new List<FoodCheckBoxModel>();
+			selectedBreakfast = new List<FoodModel>();
+			selectedLunch = new List<FoodModel>();
+			selectedDinner = new List<FoodModel>();
 
 
             menu = new List<FoodIntakeModel>()
@@ -235,14 +235,15 @@ namespace KenoAssist.Web.Controllers
 
             return View(currentMenu);
         }
-
-        [HttpPost]
-        public IActionResult Menu(FoodIntakeModel food,int addDays = 0)
+       
+        public IActionResult MenuSubmit(string Date)
         {
 
-            DateTime day = DateTime.Now.AddDays(addDays);
+			DateTime day = Convert.ToDateTime(Date);
 
-            var currentMenu = menu.Where(f => f.Date.Day.Equals(day.Day)).FirstOrDefault();
+			FoodIntakeModel food = HttpContext.Session.GetObjectFromJson<FoodIntakeModel>("NewMenuSelection");
+
+			var currentMenu = food;
 
             if (currentMenu == null)
             {
@@ -259,69 +260,78 @@ namespace KenoAssist.Web.Controllers
 
 
             ViewBag.IsSubmitted = IsSubmitted;
-            ViewBag.DayCount = addDays;
+			ViewBag.DayCount = day.Day - DateTime.Now.Day;
             ViewBag.CurrentDay = Helper.Common.GetDayName(day);
 
-            return View(currentMenu);
+            return View("Menu",currentMenu);
         }
-
-        public IActionResult MealSelection(DateTime Date)
+        
+        public IActionResult MealSelection(string Date)
         {
             ViewBag.PageTitle = "Breakfast";
             ViewBag.Date = Date;
             ViewBag.PageNo = 1;
-            return View(breakfastCheckbox);
+
+			var mealItems = new MealSelectionModel();
+			mealItems.Mains = breakfastList.Where(m => m.FoodTypeId == 1).ToList();
+			mealItems.Sides = breakfastList.Where(m => m.FoodTypeId == 2).ToList();
+
+			return View(mealItems);
         }
 
         [HttpPost]
-        public IActionResult MealSelection(IEnumerable<FoodCheckBoxModel> SelectedFood,DateTime Date, int PageNo)
+		public IActionResult MealSelection(MealSelectionModel model,string date, int pageNo)
         {
-            switch(PageNo){
+			switch(pageNo){
                 case 1:
                     ViewBag.PageTitle = "Breakfast";
-                    ViewBag.PageNo = PageNo;
-                    return View(breakfastCheckbox);
+					ViewBag.PageNo = pageNo;
+					var breakfastItems = new MealSelectionModel();
+					breakfastItems.Mains = breakfastList.Where(m => m.FoodTypeId == 1).ToList();
+					breakfastItems.Sides = breakfastList.Where(m => m.FoodTypeId == 2).ToList();
+					return View(breakfastItems);
                 case 2:
                     ViewBag.PageTitle = "Lunch";
-                    ViewBag.PageNo = PageNo;
-                    HttpContext.Session.SetObjectAsJson("Breakfast", SelectedFood.Where(m => m.IsChecked).ToList());
-                    SelectedFood = new List<FoodCheckBoxModel>();
-                    return View(lunchCheckbox);
+					ViewBag.Date = date;
+					ViewBag.PageNo = pageNo;
+					HttpContext.Session.SetObjectAsJson("Breakfast", breakfastList.Where(m => m.Id == model.SelectedMain || m.Id == model.SelectedSide).ToList());
+					ModelState.Clear();
+					var lunchItems = new MealSelectionModel();
+					lunchItems.Mains = lunchList.Where(m => m.FoodTypeId == 1).ToList();
+					lunchItems.Sides = lunchList.Where(m => m.FoodTypeId == 2).ToList();
+					return View(lunchItems);
                 case 3:
                     ViewBag.PageTitle = "Dinner";
-                    ViewBag.PageNo = PageNo;
-                    HttpContext.Session.SetObjectAsJson("Lunch", SelectedFood.Where(m => m.IsChecked).ToList());
-                    SelectedFood = new List<FoodCheckBoxModel>();
-                    return View(dinnerCheckbox);
+					ViewBag.Date = date;
+					ViewBag.PageNo = pageNo;
+					HttpContext.Session.SetObjectAsJson("Lunch", lunchList.Where(m => m.Id == model.SelectedMain || m.Id == model.SelectedSide).ToList());
+					ModelState.Clear();
+					var dinnerItems = new MealSelectionModel();
+					dinnerItems.Mains = dinnerList.Where(m => m.FoodTypeId == 1).ToList();
+					dinnerItems.Sides = dinnerList.Where(m => m.FoodTypeId == 2).ToList();
+					return View(dinnerItems);
                 case 4:
-                    var food = new FoodIntakeModel();
-                    food.Breakfast = new List<FoodModel>();
-                    food.Lunch = new List<FoodModel>();
-                    food.Dinner = new List<FoodModel>();
+                    var _food = new FoodIntakeModel();
+					_food.Breakfast = new List<FoodModel>();
+					_food.Lunch = new List<FoodModel>();
+					_food.Dinner = new List<FoodModel>();
 
-                    selectedBreakfast = HttpContext.Session.GetObjectFromJson<List<FoodCheckBoxModel>>("Breakfast");
-                    selectedLunch = HttpContext.Session.GetObjectFromJson<List<FoodCheckBoxModel>>("Lunch");
-                    selectedDinner = SelectedFood.Where(m => m.IsChecked).ToList();
+                    selectedBreakfast = HttpContext.Session.GetObjectFromJson<List<FoodModel>>("Breakfast");
+					selectedLunch = HttpContext.Session.GetObjectFromJson<List<FoodModel>>("Lunch");
+					selectedDinner = dinnerList.Where(m => m.Id == model.SelectedMain || m.Id == model.SelectedSide).ToList();
+                                  
+					_food.Breakfast.AddRange(selectedBreakfast);
+					_food.Lunch.AddRange(selectedLunch);
+					_food.Dinner.AddRange(selectedDinner);
 
-                    foreach(var item in selectedBreakfast){
-                        food.Breakfast.Add(Common.MapCheckBoxToFood(item));
-                    }
 
-                    foreach (var item in selectedLunch)
-                    {
-                        food.Lunch.Add(Common.MapCheckBoxToFood(item));
-                    }
+					HttpContext.Session.SetObjectAsJson("NewMenuSelection", _food);
 
-                    foreach (var item in selectedDinner)
-                    {
-                        food.Dinner.Add(Common.MapCheckBoxToFood(item));
-                    }
-
-                    return RedirectToAction("Menu", food);
+					return RedirectToAction("MenuSubmit", new {Date = date});
                 default:
                     return View();
             }
         }
-
+        
     }
 }
--- KenoAssist.Web/Models/MealSelectionModel.cs
+++ KenoAssist.Web/Models/MealSelectionModel.cs
@@ -1,8 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace KenoAssist.Web.Models
 {
     public class MealSelectionModel
     {
-        
+		public List<FoodModel> Mains { get; set; }
+		public List<FoodModel> Sides { get; set; }
+		public long SelectedMain { get; set; }
+		public long SelectedSide { get; set; }
     }
 }
