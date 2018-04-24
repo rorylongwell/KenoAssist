2b5964873d427c98e3edd9a2b3c0dcc835c7fbf9
rorylongwell <rory.longwell@gmail.com>
2018-04-24 22:55:16 +0100

    Drinks improvements

--- KenoAssist.Web/Models/DrinkSelectionModel.cs
+++ KenoAssist.Web/Models/DrinkSelectionModel.cs
@@ -10,26 +10,7 @@ namespace KenoAssist.Web.Models
     // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
     public class DrinkSelectionModel
     {
-        private readonly RequestDelegate _next;
-
-        public DrinkSelectionModel(RequestDelegate next)
-        {
-            _next = next;
-        }
-
-        public Task Invoke(HttpContext httpContext)
-        {
-
-            return _next(httpContext);
-        }
-    }
-
-    // Extension method used to add the middleware to the HTTP request pipeline.
-    public static class DrinkSelectionModelExtensions
-    {
-        public static IApplicationBuilder UseMiddlewareClassTemplate(this IApplicationBuilder builder)
-        {
-            return builder.UseMiddleware<DrinkSelectionModel>();
-        }
+		public List<DrinkModel> Drinks { get; set;}
+		public long SelectedDrink {get; set;}
     }
 }
